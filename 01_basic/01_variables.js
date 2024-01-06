12d81a087e35864c9636f4fce0cef1e5f6bf4093
DanielPurna <78731340+DanielPurna@users.noreply.github.com>
2024-01-06 12:38:12 +0000

    variable 01 done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const userId = 87002;
+let userEmail = "daniel@google.com";
+var userPassword = "12345";
+userStatic = "ok"
+
+
+/* 
+var kahile pani use nagarnus never use var 
+jahile pani js ma varable ko lagi use garda const ya ta let ko prayog garnuparcha
+
+*/
+
+
+console.table([userId, userEmail, userPassword, userStatic]);
