2618c85069120ac9106c0328348e6757af62ba50
DanielPurna <78731340+DanielPurna@users.noreply.github.com>
2024-01-07 00:58:08 +0000

    updated

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -7,8 +7,11 @@ userStatic = "ok"
 /* 
 var kahile pani use nagarnus never use var 
 jahile pani js ma varable ko lagi use garda const ya ta let ko prayog garnuparcha
+beacause of block scoope and functional scoope
 
 */
-
+// userEmail = "daniel@gmail.com"
+// userPassword = "00000"
+// userStatic = "No"
 
 console.table([userId, userEmail, userPassword, userStatic]);
