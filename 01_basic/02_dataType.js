31fe6ce7349e0fe73478a8a2dda688048eb3f2ed
DanielPurna <78731340+DanielPurna@users.noreply.github.com>
2024-01-07 01:24:31 +0000

    dataTypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; // we are using newyer version yo lekhda pani hunchha nalekhda pani hunchha
+
+
+// alert(3+3); // we can not see output in nodejs but we can see in browser
+
+
+let name = "Purna"
+let age = 35
+let idLoggedIn = true
+
+
+// DataTypes 
+
+//string => "Purna"
+// number => 2
+// boolean => true/false
+//bigInt = dherai bhanda dherai number use garnalai
+// null = standbolen khali thau
+// undefined = undefied kehi nabhaeko
+//symbol = unique garaunalai use garinchha 
+
+console.log(typeof null); // type object
+console.log(typeof undefined); // type undefiend
