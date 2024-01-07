af321c397f3d17d2b35785499e1aebafe6afc66a
DanielPurna <78731340+DanielPurna@users.noreply.github.com>
2024-01-07 16:29:01 +0000

    math and number

---
+++ 01_basic/05_number_maths.js
@@ -0,0 +1,45 @@
+// const newNumber = new Number(200);
+
+// console.log(newNumber);
+
+
+// console.log(newNumber.toString().length);
+// console.log(newNumber.toFixed(2));
+
+
+// const anotherNumber = 189.565658
+
+// console.log(anotherNumber.toPrecision(3));
+// console.log(anotherNumber.toPrecision(6));
+
+
+// const hundres = 1000000
+
+// console.log(hundres.toLocaleString("en-IN"));
+
+
+
+// // ++++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(10.5));
+// console.log(Math.ceil(10.5));
+// console.log(Math.floor(10.5));
+// console.log(Math.min(10,15,6,2,80));
+// console.log(Math.max(10,15,6,2,80));
+
+
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min + 1) + min));
+
+
+
+
