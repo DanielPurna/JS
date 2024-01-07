6889dfc2fd03b9291f059a98c59cf59a937c1ff7
DanielPurna <78731340+DanielPurna@users.noreply.github.com>
2024-01-07 09:23:47 +0000

    done

---
+++ 01_basic/03_conversionDataType&Operators.js
@@ -0,0 +1,37 @@
+let score = 99
+
+
+let scoreNymber = Number(score)
+console.log( typeof scoreNymber);
+
+let changeNumberIntoString = String(score);
+console.log(typeof changeNumberIntoString);
+
+
+// kunai pani number athawa string lai hami change garna sakchhau string ko lagi String number agadi Number lagayar
+
+
+let userLogIn = 1
+console.log(typeof userLogIn);
+
+
+
+// ***************Oprators***********
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+console.log("2" +1);
+console.log(5 +"2");
+console.log(5 +5 + "5");
+console.log("5" +5 + 5);
+
+// opration ma jahile phahilo lekheko lai 
+//jodinchha pachhi lekhieko lai jodidaina yadi tyo number nai bhaya pani
+
+
